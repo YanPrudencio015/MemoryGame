dbf7936f86b4bb7d046e196646b93f7eafa727c8
Yan Pereira Prudencio <yanpereira015@gmail.com>
2022-10-15 16:17:17 -0300

    this is the project

---
+++ game.js
@@ -0,0 +1,125 @@
+const FRONT = "card_front"
+const BACK = "card_back"
+const ICON = "icon"
+
+const CARD = "card"
+
+
+startGame()
+function startGame(){
+
+ initializeCards( game.createCardsFromtechs());
+ 
+}
+
+
+
+
+
+
+function initializeCards(cards){
+    let gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = '';
+
+    game.cards.forEach(card =>{
+    let cardElement = document.createElement('div');
+    cardElement.id = card.id;
+    cardElement.classList.add(CARD);
+    cardElement.dataset.icon = card.icon;
+
+    createCardContent(card, cardElement)
+
+    cardElement.addEventListener('click', flipCard )
+    gameBoard.appendChild(cardElement);
+
+   
+   
+})
+}
+
+
+function createCardContent(card, cardElement){
+
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+
+}
+
+function createCardFace(face, card, element){
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if(face === FRONT){
+        let inconElement = document.createElement('img');
+        inconElement.classList.add(ICON);
+        inconElement.src = "./imgs/" + card.icon + ".png"
+        cardElementFace.appendChild(inconElement);
+
+    }else{
+        cardElementFace.innerHTML = "Memory";
+    }
+    element.appendChild(cardElementFace);
+}
+
+function flipCard(){
+
+
+
+   
+  
+    if(game.setCard(this.id)){
+    this.classList.add("flip");
+
+    if(game.secondCard){
+        
+       
+        if(game.checkMatch()){
+            
+            game.clearCards();
+           
+          
+
+            setTimeout(()=>{
+                if(game.checkGameOver()){
+                let gameOverLayer = document.getElementById("gameOver");
+                gameOverLayer.style.display = 'flex';
+            }},900);
+            
+        }else{
+            setTimeout(()=>{  
+                let firstCardView = document.getElementById(game.firstCard.id);
+                let secondCardview = document.getElementById(game.secondCard.id);
+                
+                firstCardView.classList.remove('flip');
+                secondCardview.classList.remove('flip');
+                game.unflipedCards()
+                },800);
+               
+          
+        }; 
+
+       
+
+       
+    }
+}
+
+}
+
+
+
+
+function restart(){
+    game.clearCards();
+    startGame();
+
+    let gameOverLayer = document.getElementById("gameOver");
+    gameOverLayer.style.display = 'none';
+
+
+
+}
+
+
+
---
+++ gameLogica.js
@@ -0,0 +1,192 @@
+
+
+let game = {
+
+        
+    // playertime:0,
+    // jogadas:0,
+    // player1:0,
+    // player2:0,
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+    cards: null,
+
+    techs: ['apple','burgerking',],
+    // 'coca','kfc',
+    // 'mcdonald','pepsi','bbc','land',
+    // 'nike','champios','warner','vinga',
+    //             'superman'],
+
+
+
+
+ 
+
+    setCard: function (id){
+        let card = this.cards.filter(card => card.id === id)[0];
+        
+        if (card.flipped || this.lockMode){
+            return false;
+        }
+        if (!this.firstCard){
+            this.firstCard = card;
+            this.firstCard.flipped=true;
+            return true;
+        }else{
+            this.secondCard = card;
+            this.secondCard.flipped = true
+            this.lockMode = true;
+            return true;
+        }
+
+    },
+
+    checkMatch: function(){
+        if(!this.firstCard||!this.secondCard){
+            return false
+
+            
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+        
+    
+        
+           
+           
+     
+    },
+
+    clearCards: function(){
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+
+    unflipedCards(){
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards()
+        
+    },
+
+    checkGameOver(){
+        return this.cards.filter(card => !card.flipped).length == 0;
+
+    },
+           createCardsFromtechs: function(){
+             this.cards = []
+
+             
+        
+             this.techs.forEach((tech)=>{
+                this.cards.push(this.createPairFromTech(tech));
+            }) 
+        
+            this.cards =  this.cards.flatMap(pair => pair)
+            this.shuffleCards();
+            return this.cards
+        },
+        
+        createPairFromTech:function(tech){
+            return [{
+                id: this.createIdWithTech(tech),
+                icon:tech,
+                flipped: false,
+                
+            },{
+                id: this.createIdWithTech(tech),
+                icon:tech,
+                flipped: false,
+        
+            }]
+
+      
+        },
+        
+        createIdWithTech: function(tech){
+            return tech + parseInt(Math.random()*1000);
+        },
+
+        shuffleCards: function (cards){
+            let currentIndex = this.cards.length;
+            let randomIndex = 0;
+        
+            while (currentIndex !== 0){
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex --;
+        
+                [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]] 
+            }
+
+        },
+
+
+
+        
+      // criar uma forma de verificar se quem fez a jogada foi o primeiro jogador ou o segundo
+      /*criar um contador de jogadas e verificar, com o número de pares das cartas viradas, quem jogou foi o primeiro ou o segundo
+        emprimir no console.log qual é a vez detre os jogadores.
+      */
+
+    playerTime:function(player1,player2, card1, card2){
+       
+},
+    // função para verificar se as duas cartas que foram viradas são ou não pares
+            // cada virada, somar +1 o número de jogas
+// playerplays: function( id){
+
+// let card = this.cards.filter( card=>(card.id ===id));
+
+// let card1 = card;
+// let card2 = card;
+
+// let madePair = 0;
+
+//  if(card1 == card2){
+
+// madePair = card1==card2;
+
+// console.log(" houve uma formação de par")
+//  } 
+
+//  console.log( madePair)
+
+
+
+ 
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     card1Id = this.createPairFromTech(tech);
+//     card2Id = this.createPairFromTech(tech);
+
+//     if(card1Id = card2Id){
+        
+//         console.log("você fez par")
+
+//     }else{
+//             console.log("próximo jogador")
+        
+//     }
+
+
+
+
+        
+}
